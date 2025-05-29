171287e01a0755c67f3b3b496059a594f54c50fd
maksymyankovych <yankovych1604@gmail.com>
2025-05-29 03:11:59 +0300

    Final

--- subresale-website-frontend/src/environments/environment.dev.ts
+++ subresale-website-frontend/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BACKEND_PRIVATE_URL: 'https://course-website-yepd.onrender.com/api/',
-  BACKEND_PUBLIC_URL: 'https://course-website-yepd.onrender.com/public-api/',
+  BACKEND_PRIVATE_URL: 'https://subresale-website.onrender.com/api/',
+  BACKEND_PUBLIC_URL: 'https://subresale-website.onrender.com/public-api/',
 };
